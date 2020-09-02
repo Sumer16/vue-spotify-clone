9096b5e5d0d679c478cd982e96bbaf19076920c6
Sumer Patel <sumerdpatel@gmail.com>
2020-09-02 10:46:39 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: 'Proxima Nova',
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        'green': '#1DB954',
+        'dark': '#121212',
+        'light': '#282828',
+        'lightest': '#B3B3B3',
+        'darkest': '#191414' 
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
